7dce3acaa62482eed2e46ec97bb0fc1b14f6ec82
Harikeshav R <r.harikeshav@gmail.com>
2023-11-23 18:46:10 +0530

    Convert to static methods

--- CipherVault/generator.py
+++ CipherVault/generator.py
@@ -8,38 +8,34 @@ import string
 
 
 class Generator:
-    def __init__(
-            self,
-            password_length: int,
-            minimum_uppercase_characters: int,
-            minimum_numerical_characters: int,
-            minimum_special_characters: int
-    ):
-        self.password_length = password_length
-        self.minimum_uppercase_characters = minimum_uppercase_characters
-        self.minimum_numerical_characters = minimum_numerical_characters
-        self.minimum_special_characters = minimum_special_characters
-        self.data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    def __init__(self):
+        pass
 
-    def generate_username(self):
+    @staticmethod
+    def generate_username():
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
         noun = random.choice(
-            open(os.path.join(self.data_dir, 'nouns.txt'), 'r').read().strip().split()).lower().capitalize()
+            open(os.path.join(data_dir, 'nouns.txt'), 'r').read().strip().split()).lower().capitalize()
         adjective = random.choice(
-            open(os.path.join(self.data_dir, 'adjectives.txt'), 'r').read().strip().split()).capitalize()
+            open(os.path.join(data_dir, 'adjectives.txt'), 'r').read().strip().split()).capitalize()
         number = str(random.randint(1, 99))
 
         return adjective + noun + number
 
-    def generate_password(self):
+    @staticmethod
+    def generate_password(password_length: int,
+                          minimum_uppercase_characters: int,
+                          minimum_numerical_characters: int,
+                          minimum_special_characters: int):
         character_set = string.ascii_letters + string.digits + string.punctuation
 
         while True:
-            password = ''.join(secrets.choice(character_set) for _ in range(self.password_length))
-            if (sum(c.isupper() for c in password) >= self.minimum_uppercase_characters
-                and sum(c.isdigit() for c in password) >= self.minimum_numerical_characters) \
-                    and sum(True for c in password if c in string.punctuation):
+            password = ''.join(secrets.choice(character_set) for _ in range(password_length))
+            if (sum(c.isupper() for c in password) >= minimum_uppercase_characters
+                and sum(c.isdigit() for c in password) >= minimum_numerical_characters) \
+                    and sum(True for c in password if c in string.punctuation) >= minimum_special_characters:
                 return password
-
 
 # generator = Generator(16, 2, 3, 4)
 # print(generator.generate_username())
